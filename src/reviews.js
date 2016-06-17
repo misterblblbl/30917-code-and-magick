0e79e3b45755b7dd38d35df50091b3ae2d7e1e56
Alexandra Godun <mister.blblbl@gmail.com>
2016-06-17 16:41:26 +0300

    Добавлена загрузка данных через xhr, добавлена фильтрация отзывов

--- src/reviews.js
+++ src/reviews.js
@@ -3,8 +3,13 @@
 (function() {
   var reviewsFilters = document.querySelector('.reviews-filter');
   var reviewsContainer = document.querySelector('.reviews-list');
+  var reviewsBlock = document.querySelector('.reviews');
+
+  var reviewsFiltersList = reviewsFilters.querySelectorAll('.reviews-filter-item');
+
   var template = document.getElementById('review-template');
   var elementToClone;
+  var reviews = [];
 
   if('content' in template) {
     elementToClone = template.content.querySelector('.review');
@@ -12,8 +17,21 @@
     elementToClone = template.querySelector('.review');
   }
 
+  /** @constant {string} */
+  var REVIEWS_LOAD_URL = 'https://o0.github.io/assets/json/reviews.json';
+
   /** @constant {number} */
   var IMAGE_LOAD_TIMEOUT = 10000;
+
+  var Filter = {
+    'ALL': 'reviews-all',
+    'BAD': 'reviews-bad',
+    'GOOD': 'reviews-good',
+    'RECENT': 'reviews-recent',
+    'POPULAR': 'reviews-popular'
+  };
+
+  var DEFAULT_FILTER = Filter.ALL;
 
 
   /**
@@ -51,33 +69,96 @@
     return element;
   };
 
-
-  var loadJSONPData = function(url, callbackAfter, callbackBefore) {
+  var getReviewsData = function(callbackAfter, callbackBefore) {
     if(typeof callbackBefore === 'function') {
       callbackBefore();
     }
+    var xhr = new XMLHttpRequest();
 
-    var scriptElement = document.createElement('script');
-    scriptElement.src = url;
-    document.body.appendChild(scriptElement);
-
-    window.__reviewsLoadCallback = function(data) {
-      if(typeof callbackAfter === 'function') {
-        callbackAfter(data);
-      }
+    xhr.onload = function(evt) {
+      var loadedData = JSON.parse(evt.target.response);
+      callbackAfter(loadedData);
     };
+
+    xhr.ontimeout = function () {
+      reviewsBlock.classList.remove('reviews-list-loading');
+      reviewsBlock.classList.add('reviews-load-failure');
+    };
+
+    xhr.timeout = IMAGE_LOAD_TIMEOUT;
+    xhr.open('GET', REVIEWS_LOAD_URL);
+    xhr.send();
   };
 
-  loadJSONPData('https://up.htmlacademy.ru/assets/js_intensive/jsonp/reviews.js', function(data) {
-    data.forEach(function(review) {
-      getReviewElement(review);
-    });
-
-    //Отображаем блок с фильтрами.
-    reviewsFilters.classList.remove('invisible');
-  }, function() {
-
+  var showPreloader = function() {
+    //Пока длится загрузка файла, показываем прелоадер
+    reviewsBlock.classList.add('reviews-list-loading');
     //Прячем блок с фильтрами .reviews-filter
     reviewsFilters.classList.add('invisible');
-  });
+  };
+
+  var removePreloader = function() {
+    //Убираем прелоадер
+    reviewsBlock.classList.remove('reviews-list-loading');
+    //Отображаем блок с фильтрами.
+    reviewsFilters.classList.remove('invisible');
+  };
+
+  var renderReviews = function(reviews) {
+    reviewsContainer.innerHTML = '';
+    reviews.forEach(function(review) {
+      getReviewElement(review);
+    });
+  };
+
+  var getFilteredReviews = function(reviewsList, filter) {
+    var reviewsToFilter = reviewsList.slice(0);
+
+    switch (filter) {
+      case Filter.BAD:
+        reviewsToFilter.sort(function(a, b) {
+          return a.rating - b.rating;
+        });
+        break;
+      case Filter.GOOD:
+        reviewsToFilter.sort(function(a, b) {
+          return b.rating - a.rating;
+        });
+        break;
+      case Filter.RECENT:
+        reviewsToFilter.sort(function(a, b) {
+          return new Date(b.date) - new Date(a.date);
+        });
+        break;
+      case Filter.POPULAR:
+        reviewsToFilter.sort(function(a, b) {
+          return b.review_usefulness - a.review_usefulness;
+        });
+        break;
+    }
+    return reviewsToFilter;
+  };
+
+  var setFilter = function(filter) {
+    var filteredReviews = getFilteredReviews(reviews, filter);
+    renderReviews(filteredReviews);
+  };
+
+  var setFiltrationEnabled = function(enabled) {
+    for (var i = 0; i < reviewsFiltersList.length; i++) {
+      reviewsFiltersList[i].onclick = enabled ? function(evt) {
+        setFilter(this.getAttribute('for'));
+      } : null;
+    }
+  };
+
+  showPreloader();
+  getReviewsData(function(loadedReviews) {
+    reviews = loadedReviews;
+    setFiltrationEnabled(true);
+    setFilter(Filter.ALL);
+  },
+    removePreloader()
+  );
+
 })();
