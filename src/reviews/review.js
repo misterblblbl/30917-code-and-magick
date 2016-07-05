80d2a3c51f2d5f387337641da1df1efce9cb6076
Alexandra Godun <mister.blblbl@gmail.com>
2016-07-05 19:29:05 +0300

    Исправила обработчки кликов в блоке голосования ♿️

--- src/reviews/review.js
+++ src/reviews/review.js
@@ -12,21 +12,34 @@ var Review = function(data, container) {
   this.data = data;
 
   this.element = getReviewElement(this.data);
+  container.appendChild(this.element);
 
-  this.onReviewClick = function() {
+  /** @type {HTMLElement} */
+  this.reviewQuizYes = this.element.querySelector('.review-quiz-answer-yes');
 
-    /** @type {HTMLElement} */
-    var reviewQuiz = document.querySelector('.review-quiz-answer');
-    reviewQuiz.classList.add('review-quiz-answer-active');
-  };
+  /** @type {HTMLElement} */
+  this.reviewQuizNo = this.element.querySelector('.review-quiz-answer-no');
+
+  this.onReviewClickYes = this.onReviewClickYes.bind(this);
+  this.onReviewClickNo = this.onReviewClickNo.bind(this);
+
+  this.reviewQuizYes.addEventListener('click', this.onReviewClickYes);
+  this.reviewQuizNo.addEventListener('click', this.onReviewClickNo);
 
   this.remove = function() {
     this.element.removeEventListener('click', this.onReviewClick);
     this.element.parentNode.removeChild(this.element);
   };
+};
 
-  this.element.addEventListener('click', this.onReviewClick);
-  container.appendChild(this.element);
+Review.prototype.onReviewClickYes = function() {
+  this.reviewQuizYes.classList.add('review-quiz-answer-active');
+  this.reviewQuizNo.classList.remove('review-quiz-answer-active');
+};
+
+Review.prototype.onReviewClickNo = function() {
+  this.reviewQuizNo.classList.add('review-quiz-answer-active');
+  this.reviewQuizYes.classList.remove('review-quiz-answer-active');
 };
 
 module.exports = Review;
